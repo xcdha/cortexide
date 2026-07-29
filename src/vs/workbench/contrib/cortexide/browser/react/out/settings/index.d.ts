7bbbae975a0825c77d4690402fef1968c4d10f69
xcdha <xcdha@users.noreply.github.com>
2026-07-29 11:44:21 +0000

    feat: 项目分析
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- src/vs/workbench/contrib/cortexide/browser/react/out/settings/index.d.ts
+++
@@ -1,8 +0,0 @@
-/*--------------------------------------------------------------------------------------
- *  Copyright 2025 Glass Devtools, Inc. All rights reserved.
- *  Licensed under the Apache License, Version 2.0. See LICENSE.txt for more information.
- *--------------------------------------------------------------------------------------*/
-
-import { ServicesAccessor } from '../../../../../../../platform/instantiation/common/instantiation.js';
-
-export declare function mountVoidSettings(container: HTMLElement, accessor: ServicesAccessor): { dispose: () => void } | null;
